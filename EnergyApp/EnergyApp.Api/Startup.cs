d15cba4c1afa65b278296756d7bd4d98bda50615
Lucas Felix Carvalho <lucas.felixcarvalho@hotmail.com>
2021-09-06 11:02:24 -0300

    Implemented log with cross-currint concern for EventService

--- EnergyApp/EnergyApp.Api/Startup.cs
+++ EnergyApp/EnergyApp.Api/Startup.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Castle.DynamicProxy;
 using EnergyApp.Domain.Event;
+using EnergyApp.Domain.Logger;
 using EnergyApp.Domain.Meter;
 using EnergyApp.Infra.Repository;
 using Microsoft.AspNetCore.Builder;
@@ -29,9 +31,13 @@ namespace EnergyApp.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IMeterRepository, Memory_MeterRepository>();            
-            services.AddScoped<IEventService, EventService>();
+            services.AddSingleton<IMeterRepository, Memory_MeterRepository>();
+            services.AddSingleton(new ProxyGenerator());
+
+            services.AddScoped<IInterceptor, Logger>();            
             services.AddScoped<IMeterService, MeterService>();
+
+            AddLoggedScoped<IEventService, EventService>(services);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -59,6 +65,20 @@ namespace EnergyApp.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        private void AddLoggedScoped<TInterface, TImplementation>(IServiceCollection services)
+        where TInterface : class
+        where TImplementation : class, TInterface
+        {
+            services.AddScoped<TImplementation>();
+            services.AddScoped(typeof(TInterface), serviceProvider =>
+            {
+                var proxyGenerator = serviceProvider.GetRequiredService<ProxyGenerator>();
+                var actual = serviceProvider.GetRequiredService<TImplementation>();
+                var interceptors = serviceProvider.GetServices<IInterceptor>().ToArray();
+                return proxyGenerator.CreateInterfaceProxyWithTarget(typeof(TInterface), actual, interceptors);
             });
         }
     }
